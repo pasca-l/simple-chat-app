650d7eb6082f48fe4b4de06f72516f794e1c0294
pasca-l <shion.yamadate@keio.jp>
2022-08-29 17:45:08 +0900

    renewed bottom navigation bar

--- app/lib/views/user_home.dart
+++ app/lib/views/user_home.dart
@@ -16,106 +16,61 @@ class UserHome extends StatefulWidget {
 class _UserHomeState extends State<UserHome> {
   final user = FirebaseAuth.instance.currentUser!;
 
+  int pageIndex = 0;
   final pages = [
     HomePage(),
     MessagePage(),
     ProfilePage(),
   ];
-  final ValueNotifier<int> pageIndex = ValueNotifier(0);
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: userAppBar(),
       drawer: menuDrawer(),
       bottomNavigationBar: _homeNavigationBar(
-        onIndexChange: (index) {
-          pageIndex.value = index;
-        }
-      ),
-      body: ValueListenableBuilder(
-        valueListenable: pageIndex,
-        builder: (context, int value, child) {
-          return pages[value];
+        selectedPage: pageIndex,
+        onTap: (int index) {
+          setState(() {
+            pageIndex = index;
+          });
         },
       ),
+      body: pages.elementAt(pageIndex),
     );
-
   }
 }
 
 class _homeNavigationBar extends StatelessWidget {
   const _homeNavigationBar({
     Key? key,
-    required this.onIndexChange,
-  }) : super(key: key);
-
-  final ValueChanged<int> onIndexChange;
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      top: false,
-      bottom: true,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          _navigationBarItem(
-            label: "home",
-            icon: Icons.home,
-            onTap: () {
-              onIndexChange(0);
-            }
-          ),
-
-          _navigationBarItem(
-            label: "message",
-            icon: Icons.message,
-            onTap: () {
-              onIndexChange(1);
-            }
-          ),
-
-          _navigationBarItem(
-            label: "profile",
-            icon: Icons.accessibility,
-            onTap: () {
-              onIndexChange(2);
-            }
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class _navigationBarItem extends StatelessWidget {
-  const _navigationBarItem({
-    Key? key,
-    required this.label,
-    required this.icon,
+    required this.selectedPage,
     required this.onTap,
   }) : super(key: key);
 
-  final String label;
-  final IconData icon;
-  final VoidCallback onTap;
+  final int selectedPage;
+  final onTap;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      behavior: HitTestBehavior.opaque,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Icon(icon),
-          SizedBox(height: 4),
-          Text(label),
+    return BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: "message",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.accessibility),
+            label: "profile",
+          ),
         ],
-      ),
+        currentIndex: selectedPage,
+        selectedItemColor: Colors.amber[800],
+        onTap: onTap,
     );
-    
   }
 }
